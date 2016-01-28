9ab5c2bfd20d6183a693a4a48df941a35681ed74
Sergey Popov <sergobot@protonmail.com>
2016-01-28 19:56:00 +0300

    Add basic game class

---
+++ core/Game.cpp
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) Sergey Popov <sergobot@protonmail.com>
+ * See LICENSE for details.
+**/
+
+#include "Game.h"
+
+namespace Rocky {
+Game::Game()
+{
+    
+}
+
+void Game::start()
+{
+    
+}
+}
---
+++ core/Game.h
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) Sergey Popov <sergobot@protonmail.com>
+ * See LICENSE for details.
+**/
+
+#ifndef ROCKYCORE_GAME_H
+#define ROCKYCORE_GAME_H
+
+namespace Rocky {
+class Game
+{
+public:
+    Game();
+    
+    void start();
+};
+}
+
+#endif // ROCKYCORE_GAME_H
