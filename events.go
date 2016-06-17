5cafb53697f5fcf97927b1c0140e2ebf71336b1b
Sergey Popov <sergobot@protonmail.com>
2016-06-17 20:27:04 +0300

    Encapsulate events' data better
    This commit makes KeyEvents' and MouseButtonEvents' data private and adds some methods to set/get it.

--- events.go
+++ events.go
@@ -79,10 +79,10 @@ func (be *BasicEvent) Process() {
 // - Modifiers (Shift, Alt and so on).
 type KeyEvent struct {
 	BasicEvent
-	Key      glfw.Key
-	Scancode int
-	Action   glfw.Action
-	Mods     glfw.ModifierKey
+	key      glfw.Key
+	scancode int
+	action   glfw.Action
+	mods     glfw.ModifierKey
 }
 
 // Type returns type of an event. For KeyEvent it is KeyEventType.
@@ -90,12 +90,52 @@ func (ke *KeyEvent) Type() EventType {
 	return KeyEventType
 }
 
+// SetKey method sets KeyEvent's .key
+func (ke *KeyEvent) SetKey(k glfw.Key) {
+	ke.key = k
+}
+
+// Key method of KeyEvent struct returns its .key
+func (ke *KeyEvent) Key() glfw.Key {
+	return ke.key
+}
+
+// SetScancode method sets KeyEvent's .scancode
+func (ke *KeyEvent) SetScancode(s int) {
+	ke.scancode = s
+}
+
+// Scancode method of KeyEvent struct returns its .scancode
+func (ke *KeyEvent) Scancode() int {
+	return ke.scancode
+}
+
+// SetAction method sets KeyEvent's .action
+func (ke *KeyEvent) SetAction(a glfw.Action) {
+	ke.action = a
+}
+
+// Action method of KeyEvent struct returns its .action
+func (ke *KeyEvent) Action() glfw.Action {
+	return ke.action
+}
+
+// SetMods method sets KeyEvent's .mods
+func (ke *KeyEvent) SetMods(m glfw.ModifierKey) {
+	ke.mods = m
+}
+
+// Mods method of KeyEvent struct returns its .mods
+func (ke *KeyEvent) Mods() glfw.ModifierKey {
+	return ke.mods
+}
+
 // Fill wraps raw GLFW data in a single Event struct
 func (ke *KeyEvent) Fill(k glfw.Key, s int, a glfw.Action, m glfw.ModifierKey) {
-	ke.Key = k
-	ke.Scancode = s
-	ke.Action = a
-	ke.Mods = m
+	ke.SetKey(k)
+	ke.SetScancode(s)
+	ke.SetAction(a)
+	ke.SetMods(m)
 }
 
 // MouseButtonEvent is a wrapper for data of GLFW's mouse button callback function
@@ -105,9 +145,9 @@ func (ke *KeyEvent) Fill(k glfw.Key, s int, a glfw.Action, m glfw.ModifierKey) {
 // - Modifiers (Shift, Alt and so on)
 type MouseButtonEvent struct {
 	BasicEvent
-	Button glfw.MouseButton
-	Action glfw.Action
-	Mods   glfw.ModifierKey
+	button glfw.MouseButton
+	action glfw.Action
+	mods   glfw.ModifierKey
 }
 
 // Type returns type of an event. For MouseButtonEvent it is MouseButtonEventType.
@@ -115,11 +155,41 @@ func (mbe *MouseButtonEvent) Type() EventType {
 	return MouseButtonEventType
 }
 
+// SetButton method sets MouseButtonEvent's .button
+func (mbe *MouseButtonEvent) SetButton(b glfw.MouseButton) {
+	mbe.button = b
+}
+
+// Button method of MouseButtonEvent struct returns its .button
+func (mbe *MouseButtonEvent) Button() glfw.MouseButton {
+	return mbe.button
+}
+
+// SetAction method sets MouseButtonEvent's .action
+func (mbe *MouseButtonEvent) SetAction(a glfw.Action) {
+	mbe.action = a
+}
+
+// Action method of MouseButtonEvent struct returns its .action
+func (mbe *MouseButtonEvent) Action() glfw.Action {
+	return mbe.action
+}
+
+// SetMods method sets MouseButtonEvent's .mods
+func (mbe *MouseButtonEvent) SetMods(m glfw.ModifierKey) {
+	mbe.mods = m
+}
+
+// Mods method of MouseButtonEvent struct returns its .mods
+func (mbe *MouseButtonEvent) Mods() glfw.ModifierKey {
+	return mbe.mods
+}
+
 // Fill wraps raw GLFW data in a single Event struct
 func (mbe *MouseButtonEvent) Fill(b glfw.MouseButton, a glfw.Action, m glfw.ModifierKey) {
-	mbe.Button = b
-	mbe.Action = a
-	mbe.Mods = m
+	mbe.SetButton(b)
+	mbe.SetAction(a)
+	mbe.SetMods(m)
 }
 
 // EventQueue strust is used to store events in queue. It has 2 methods:
