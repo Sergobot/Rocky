9aadc19e9bde601f34635691d050fcdf6d8b9120
Sergey Popov <sergobot@protonmail.com>
2016-10-02 06:35:58 +0300

    Add viewport size normalizer
    It's called NormalizedVieportSize(). Returns 2.0f for bigger side and something less for the less side, according to aspect ratio.

--- opengl/gl33/viewport.go
+++ opengl/gl33/viewport.go
@@ -4,6 +4,8 @@ package gl33
 
 import (
 	"github.com/go-gl/gl/v3.3-core/gl"
+
+	g "github.com/Sergobot/Rocky/geometry"
 )
 
 // These variables contain viewport's size in pixels. If there is any other way to get
@@ -29,6 +31,31 @@ func SetViewport(x, y, width, height int32) {
 // contribute please, it you know better solution to get OpenGL viewport.
 func Viewport() (int32, int32, int32, int32) {
 	return vpX, vpY, vpW, vpH
+}
+
+// NormalizedViewportSize returns SizeF struct containing current viewport size.
+// This size isn't in pixels or anything like that. Bigger side is always 2.0,
+// smaller is similar to viewport's one. That's made to avoid pain of dealing with
+// pixels manually.
+// For example, if viewport is exactly 1920 pixels wide and 1080 pixels tall,
+// this function will return 2.0 for width and 2.0 * 1080 / 1920 = 1.125 for height.
+// If the function is called on a phone with a FullHD screen in portrait rotation,
+// it will return 2.0 * 1080 / 1920 = 1.125 for width and 2.0 for height.
+func NormalizedViewportSize() g.SizeF {
+	var w, h float32
+
+	ratio := ViewportAspectRatio()
+	if ratio >= 1.0 {
+		// Width is bigger or equals height
+		w = 2.0
+		h = w / ratio
+	} else {
+		// Height is bigger than width.
+		h = 2.0
+		w = h * ratio
+	}
+
+	return g.SizeF{W: w, H: h}
 }
 
 // ViewportAspectRatio returns aspect ratio of viewport, calculated as Width / Height. Note,
