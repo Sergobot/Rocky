5198f05be42f09b9868f6f92911794f72b34b7d9
Sergey Popov <sergobot@protonmail.com>
2016-10-10 15:44:26 +0300

    Add Initialization state in opengl/gl33 package

--- opengl/gl33/init.go
+++ opengl/gl33/init.go
@@ -8,14 +8,25 @@ import (
 	"github.com/go-gl/gl/v3.3-core/gl"
 )
 
+// Variable to store initialization state
+var initialized bool
+
 // Init initializes OpenGL 3.3
 func Init() error {
 	if err := gl.Init(); err != nil {
+		initialized = false
 		return err
 	}
+
+	initialized = true
 
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	log.Println("Successfully initialized OpenGL version", version)
 
 	return nil
+}
+
+// Initialized returns true, if OpenGL 3.3 was successfully initialized earlier.
+func Initialized() bool {
+	return initialized
 }
